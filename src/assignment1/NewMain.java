ab1ce720f0cf61224673dd8520cb30c065d140e3
rumeysa-u <81163396+rumeysa-u@users.noreply.github.com>
2022-10-10 00:08:34 +0300

    Add files via upload
    Linkedlist

---
+++ src/assignment1/NewMain.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment1;
+
+/**
+ *
+ * @author ucarr
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PA1 A = new PA1();
+
+        A.printStatus();
+
+    }
+    
+}
