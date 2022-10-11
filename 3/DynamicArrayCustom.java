f1bcb941519f549341b31e66a28938c2e90c53aa
Yasin Arıcı <71091428+Swagghetti@users.noreply.github.com>
2022-10-11 21:55:19 +0300

    Update DynamicArrayCustom.java

--- 3/DynamicArrayCustom.java
+++ 3/DynamicArrayCustom.java
@@ -1,85 +1,89 @@
-package pa1;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class DynamicArrayCustom
 {
     private int array[];
     private int count;
     private int size;
-    private Map<String,Long> statusInformations;
-    private TimeCounter timer;
 
-    public DynamicArrayCustom() // constructor without parameter
+    public DynamicArrayCustom()
     {
         count = 0;
         size = 1;
         array = new int[1];
     }
 
-    public DynamicArrayCustom(int[] array) //dynamic array constructor with an array as parameter
+    public DynamicArrayCustom(int[] array)
     {
-        this.timer = new TimeCounter();
-        this.timer.startTimer();
         this.array = array;
-        this.statusInformations = new HashMap<String,Long>();
         size = 1;
 
-        for (int i = 0; i < array.length; i++) {  //iterate through array and insert each element
+        for (int i = 0; i < array.length; i++) {
             this.insertLast(array[i]);
         }
-        this.timer.stopTimer();
-
-        this.statusInformations.put("a",this.timer.getElepsedTime());
     }
 
-    public void insertLast(int element)    //inserts element to the last index, grows size if it's full
+    public void insertLast(int element)
     {
-        this.timer = new TimeCounter();
-        this.timer.startTimer();
-
         array[count++] = element;
 
-        if(count == size)   //if it is full
+        if(count == size)
         {
             this.doubleSize();
         }
-
-        this.timer.stopTimer();
-        this.statusInformations.put("c",this.timer.getElepsedTime());
     }
 
-    public void insertFirst(int element)  //shifts the elements and inserts to the index 0
+    public void insertFirst(int element)
     {
-        this.timer = new TimeCounter();
-        this.timer.startTimer();
-
-        if(count == 0)    // if dynamic array is empty
+        if(count == 0)
         {
             this.insertLast(element);
         }
 
-        else    //shift then insert
+        else
         {
             for (int i = count; i >= 0 ; i--) {
-                array[i+1] = array[i];
+                array[count+1] = array[count];
             }
 
             array[0] = element;
 
-            if(++count == size)  //check if the array is full, if so grow size
+            if(++count == size)
             {
                 this.doubleSize();
             }
         }
-
-        this.timer.stopTimer();
-        this.statusInformations.put("b",this.timer.getElepsedTime());
     }
 
+    public void insertAtIndex(int element, int index)
+    {
+        if(index == 0)
+        {
+            this.insertFirst(element);
+        }
+        else if(index == count)
+        {
+            this.insertLast(element);
+        }
+        else if(index > count)
+        {
+            return;
+        }
+        else
+        {
+            for (int i = count; i >= index ; i--)
+            {
+                array[count+1] = array[count];
+            }
 
-    public void doubleSize()  // method to double the current size of the array and copy its element to the newly created array with twice the size
+            array[index] = element;
+
+            if(++count == size)
+            {
+                this.doubleSize();
+            }
+        }
+    }
+
+    public void doubleSize()
     {
         size *= 2;
         int tempArray[] = new int[size];
@@ -89,30 +93,11 @@ public class DynamicArrayCustom
         array = tempArray;
     }
 
-    public int getAtIndex(int index)  // gets the element at desired index and returns, if index is invalid returns -999999
+    public int getAtIndex(int index)
     {
-        this.timer = new TimeCounter();
-        this.timer.startTimer();
-
-        if(!(index > count-1 || index < 0)){
-            this.timer.stopTimer();
-            if(index == 900_000){
-                this.statusInformations.put("d",this.timer.getElepsedTime());
-            }
-            else if (index == 9){
-                this.statusInformations.put("e",this.timer.getElepsedTime());
-            }
+        if(!(index > count-1))
             return array[index];
-        }
         else
             return -999999;
-    }
-
-    public void printStatus(){
-        System.out.println("The integer dynamic array structure is built in "+ this.statusInformations.get("a") +" milliseconds.");
-        System.out.println("An integer is inserted into the first index of the dynamic array in "+this.statusInformations.get("b")+" milliseconds.");
-        System.out.println("An integer is inserted into the 900,000th index of the dynamic array in "+this.statusInformations.get("c")+" milliseconds.");
-        System.out.println("An integer, which is "+this.getAtIndex(900000)+" , is read from the index 900,000 of the dynamic array in "+this.statusInformations.get("d")+" milliseconds.");
-        System.out.println("An integer, which is "+this.getAtIndex(9)+" , is read from the index 9 of the dynamic array in "+this.statusInformations.get("e")+" milliseconds.");
     }
 }
